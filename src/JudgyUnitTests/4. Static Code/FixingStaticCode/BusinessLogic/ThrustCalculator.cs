674b0cfdb1ae0730326a268e8faedc98b52d2a54
TJ <trentjsky@gmail.com>
2023-05-27 16:38:04 +1000

    Adding arithmetic overflow testing

--- src/JudgyUnitTests/4. Static Code/FixingStaticCode/BusinessLogic/ThrustCalculator.cs
+++ src/JudgyUnitTests/4. Static Code/FixingStaticCode/BusinessLogic/ThrustCalculator.cs
@@ -17,20 +17,32 @@ namespace FixingStaticCode.BusinessLogic
 
         /// <summary>
         /// Calculate the thrust that is required to get the given number of sloths to the moon for the chosen rocket
-        /// After lunch time, we need a little extra thrust to get through the night sky
+        /// After lunch time, we need a little extra thrust to get through the night sky.
+        /// If the thrust calculation overflows, this function will propagate the <see cref="OverflowException"/>
         /// </summary>
         public int CalculateThrust(int input1, int input2, int input3, int input4, int input5, int numberOfSloths)
         {
-            var result = (input1 + input2 + input3 + input4 + input5) * numberOfSloths;
+            try
+            {
+                checked
+                {
+                    var result = (input1 + input2 + input3 + input4 + input5) * numberOfSloths;
 
-            // We have converted from a dependency on local time to a dependency on standardised UTC time
-            if (systemClock.UtcNow.Hour <= 2)
-            {
-                return result;
+                    // We have converted from a dependency on local time to a dependency on standardised UTC time
+                    if (systemClock.UtcNow.Hour <= 2)
+                    {
+                        return result;
+                    }
+                    else
+                    {
+                        return result + 10;
+                    }
+                }
             }
-            else
+            catch(OverflowException ex)
             {
-                return result + 10;
+                // This is redundant but demonstrates that we propagate the OverFlowException
+                throw;
             }
         }
     }
